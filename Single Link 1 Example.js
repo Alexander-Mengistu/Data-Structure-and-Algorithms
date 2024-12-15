c5f83212ab0502fa001fe725369db72e7c1f075a
alex-mengistu <alex.2011@live.com.au>
2024-12-15 18:23:33 +1000

    im currently working on single link but my code not working i thought might be node.js runner so made a html version to inspect more, dont know what is the problem yet might be idention or phrasing, i will try tomoz

--- Single Link 1 Example.js
+++ Single Link 1 Example.js
@@ -1,4 +1,4 @@
-//Single Link is data strucutre that utilises arrays in chain strucutre, a signle array creates a node, contain 2 things "data" and "pointer" to the next data , and will be a pointer connecting all nodes in linear format, each node connects to the previous and next node, creating a chain, each node has its data and pointer to next node
+//Single Link is data strucutre that utilises arrays in chain strucutre, a single array creates a node, contain 2 things "data" and "pointer" to the next data , and will be a pointer connecting all nodes in linear format, each node connects to the previous and next node, creating a chain, each node has its data and pointer to next node
 //First node in linked list is called "head" and last node called "tail"
 //Single Link is traversal data structure
 
